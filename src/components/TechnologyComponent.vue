7a09f55abbb6e5f6b560e6c4a0eb2189fb7bf1fe
Cody Eklov <cody.eklov@gmail.com>
2022-03-23 16:23:44 -0500

    improve carousel button positions

--- src/components/TechnologyComponent.vue
+++ src/components/TechnologyComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="technology" class="lower-section bg-zinc-800">
     <div>
-      <h2 class="text-zinc-200">Technology Expertise</h2>
+      <h2 class="text-zinc-200">My Technology</h2>
     </div>
 
     <div>
@@ -52,13 +52,7 @@
             :src="require('@/assets/technology/' + tech.src)"
             :alt="tech.name"
           />
-          <div
-            class="
-              tech-carousel-caption
-              carousel-caption
-              text-center
-            "
-          >
+          <div class="tech-carousel-caption carousel-caption text-center">
             <h5 class="text-2xl">{{ tech.label }}</h5>
             <p class="mt-9 lg:mt-2">
               {{ tech.caption }}
@@ -81,6 +75,8 @@
           hover:outline-none hover:no-underline
           focus:outline-none focus:no-underline
           left-0
+          md:left-20
+          lg:left-28
         "
         type="button"
         data-bs-target="#tech-carousel"
@@ -107,6 +103,8 @@
           hover:outline-none hover:no-underline
           focus:outline-none focus:no-underline
           right-0
+          md:right-20
+          lg:right-28
         "
         type="button"
         data-bs-target="#tech-carousel"
@@ -156,14 +154,57 @@ export default {
         name: "Angular",
         src: "angular.svg",
         label: "Angular",
-        caption: "Another front-end JavaScript framework I have been known to use.",
+        caption:
+          "Another front-end JavaScript framework I have been known to use.",
       },
-      { id: 5, name: "HTML 5", src: "html.svg", label: "HyperText Markup Language", caption: "No surprise here. You must have some knowledge of this markup." },
-      { id: 6, name: "CSS 3", src: "css.svg", label: "Cascading Style Sheets", caption: "Websites would just be so bland without a little extra style." },
-      { id: 7, name: "Spring", src: "spring.svg", label: "Spring Framework", caption: "Spring Boot, Data, Security, and more complements my work with Java." },
-      { id: 8, name: "Microsft SQL Server", src: "ms-sql-server.svg", label: "Microsoft SQL Server", caption: "How I have mainly (but not exclusively) worked with data using SQL." },
-      { id: 9, name: "Git", src: "git.svg", label: "Git", caption: "GitHub and GitLab are my primary project repositories. I also enjoy the Git command line." },
-      { id: 10, name: "Jenkins", src: "jenkins.svg", label: "Jenkins", caption: "It's hard to develop software without a reliable source of building and deploying like Jenkins." },
+      {
+        id: 5,
+        name: "HTML 5",
+        src: "html.svg",
+        label: "HyperText Markup Language",
+        caption:
+          "No surprise here. You must have some knowledge of this markup.",
+      },
+      {
+        id: 6,
+        name: "CSS 3",
+        src: "css.svg",
+        label: "Cascading Style Sheets",
+        caption:
+          "Websites would just be so bland without a little extra style.",
+      },
+      {
+        id: 7,
+        name: "Spring",
+        src: "spring.svg",
+        label: "Spring Framework",
+        caption:
+          "Spring Boot, Data, Security, and more complements my work with Java.",
+      },
+      {
+        id: 8,
+        name: "Microsft SQL Server",
+        src: "ms-sql-server.svg",
+        label: "Microsoft SQL Server",
+        caption:
+          "How I have mainly (but not exclusively) worked with data using SQL.",
+      },
+      {
+        id: 9,
+        name: "Git",
+        src: "git.svg",
+        label: "Git",
+        caption:
+          "GitHub and GitLab are my primary project repositories. I also enjoy the Git command line.",
+      },
+      {
+        id: 10,
+        name: "Jenkins",
+        src: "jenkins.svg",
+        label: "Jenkins",
+        caption:
+          "It's hard to develop software without a reliable source of building and deploying like Jenkins.",
+      },
     ];
     return { techs };
   },
